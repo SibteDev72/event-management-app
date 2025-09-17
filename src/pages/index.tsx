f070c299d7ff867184e5fd3254a8830dd30af8a6
SibteDev72 <sibte.dev72@gmail.com>
2025-09-17 13:52:29 +0500

    I have updated the Index File

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ const HomePage = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind v3.4.1 is working 🎉
+        Tailwind v3.4.1 is working Event Management Application 🎉
       </h1>
     </div>
   );
