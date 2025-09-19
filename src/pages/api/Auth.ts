5619ee212d3770b3815bfd95cad211c191de213e
SibteDev72 <sibte.dev72@gmail.com>
2025-09-20 02:46:15 +0500

    Auth APIs Integrated

--- src/pages/api/Auth.ts
+++ src/pages/api/Auth.ts
@@ -1,19 +1,16 @@
 import { LogIn, SignUp } from "@/types/Auth";
 
+const baseURL = "http://localhost:8000";
+
 export const Signup = async (data: SignUp) => {
   try {
-    const response = await fetch("endpoint", {
+    const response = await fetch(`${baseURL}/user/signUp`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
     const result = await response.json();
     console.log("Success:", result);
     return result;
@@ -25,18 +22,13 @@ export const Signup = async (data: SignUp) => {
 
 export const Login = async (data: LogIn) => {
   try {
-    const response = await fetch("endpoint", {
+    const response = await fetch(`${baseURL}/user/logIn`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
     const result = await response.json();
     console.log("Success:", result);
     return result;
